77de40527dc15e20bca78da99cbb7a0b3ee072fe
Valeria Guerra <vguerraz@eafit.edu.co>
2022-09-06 09:36:34 -0500

    URL about sin error

--- moviereviews2/urls.py
+++ moviereviews2/urls.py
@@ -20,5 +20,5 @@ from movie import views as movieViews
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', movieViews.home),
-    path('/about', movieViews.about)
+    path('about', movieViews.about)
 ]

5c091215af7956891db0b2960a580678a1a764fa
Valeria Guerra <vguerraz@eafit.edu.co>
2022-09-06 23:22:14 -0500

    Formulario de búsqueda

--- movie/views.py
+++ movie/views.py
@@ -9,11 +9,7 @@ def home (request):
     #return render(request, 'home.html')
     #return render(request, 'home.html', {'name':'Valeria Guerra'})
     searchTerm = request.GET.get('searchMovie')
-    if searchTerm:
-        movies = Movie.objects.filter(title__icontains=searchTerm)
-    else:
-        movies = Movie.objects.all()
-    return render(request, 'home.html', {'searchterm':searchTerm, 'movies': movies})
+    return render(request, 'home.html', {'searchterm':searchTerm})
 
 def about (request):
     return render(request, 'about.html')

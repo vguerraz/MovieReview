f97c2f4966d5fcbc61299a86470e082ac87d2c47
Valeria Guerra <vguerraz@eafit.edu.co>
2022-09-06 23:09:57 -0500

    Visualizacion de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 # Create your views here.
 def home (request):
     #return HttpResponse('<h1>Welcome to Home Page </h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name':'Valeria Guerra'})
+    #return render(request, 'home.html', {'name':'Valeria Guerra'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchterm':searchTerm, 'movies': movies})
 
 def about (request):
     return render(request, 'about.html')
